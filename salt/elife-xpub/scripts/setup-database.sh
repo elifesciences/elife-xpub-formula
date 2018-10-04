f679cf7784fcaad28e4f89628c2123c5a72b62b9
Peter Hooper <diversemix@gmail.com>
2018-10-04 17:37:38 +0100

    update script

--- salt/elife-xpub/scripts/setup-database.sh
+++ salt/elife-xpub/scripts/setup-database.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
-set -e
+set -ex
 
-if ! docker-compose -f docker-compose.yml -f docker-compose.formula.yml exec -T postgres /bin/bash -c "until echo > /dev/tcp/postgres/5432; do sleep 1; done; psql -c \"SELECT 'public.entities'::regclass\""; then
-    docker-compose  -f docker-compose.yml -f docker-compose.formula.yml run app /bin/bash -c "until echo > /dev/tcp/postgres/5432; do sleep 1; done; npx pubsweet setupdb --username={{ pillar.elife_xpub.database.user }} --password={{ pillar.elife_xpub.database.password }} --email={{ pillar.elife_xpub.database.email }}"
+MAX_WAIT=20 # time in seconds
+DB_AVAILABLE_COMMAND="nc -z -w ${MAX_WAIT} ${PGHOST} ${PGPORT}"
+DC_COMMAND="docker-compose -f docker-compose.yml -f docker-compose.formula.yml"
+DB_CREATED_COMMAND="psql -c \"SELECT 'public.entities'::regclass\""
+
+SETUP_ARGS="--username={{ pillar.elife_xpub.database.user }} --password={{ pillar.elife_xpub.database.password }} --email={{ pillar.elife_xpub.database.email }}"
+
+# Run the command to ensure the database is started
+$(${DC_COMMAND})
+
+# Wait for the database to come up
+if $($(DB_AVAILABLE_COMMAND))
+then
+    if $(${DC_COMMAND} exec -T postgres /bin/bash -c "${DB_CREATED_COMMAND}")
+    then
+        $(${DC_COMMAND} run app /bin/bash -c "npx pubsweet setupdb ${SETUP_ARGS}" )
+    else
+        echo "ERROR: Database was not created: ${PGHOST} ${PGPORT}"
+    fi
+else
+    echo "ERROR: Database not available: ${PGHOST} ${PGPORT}"
 fi
 
